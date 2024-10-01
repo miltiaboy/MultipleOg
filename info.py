0e7c6b61c5e756ac476f683ca29729f7ace5c2ff
jjhgfgthfdddx <manafalthaf01@gmail.com>
2024-10-01 14:08:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,8 +41,8 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "autofilterPredvd")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://autofilterPredvd:autofilterPredvd@cluster0.ljo5dnu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://aishwaryv2:aishwaryv2@cluster0.fmybj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI3 = environ.get('DATABASE_URI3', "mongodb+srv://Leachv3:Leachv3@cluster0.rasbnes.mongodb.net/")
-DATABASE_URI4 = environ.get('DATABASE_URI4', "mongodb+srv://aishwaryv4:aishwaryv4@cluster0.afc1d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI3 = environ.get('DATABASE_URI3', "mongodb+srv://aishwaryv3:aishwaryv3@cluster0.5bipz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI4 = environ.get('DATABASE_URI4', "mongodb+srv://Renamebotv6:Renamebotv6@cluster0.xuqjwfh.mongodb.net/")
 DATABASE_URI5 = environ.get('DATABASE_URI5', "mongodb+srv://aishwaryv5:aishwaryv5@cluster0.dbh3b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
                             
 # FSUB
